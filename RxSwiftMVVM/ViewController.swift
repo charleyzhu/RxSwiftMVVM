5c595a7223e7c49cf425fc0ef4a71d77194e2a45
Charley <2555085@qq.com>
2018-11-01 13:59:45 +0800

    Initial Commit

---
+++ RxSwiftMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSwiftMVVM
+//
+//  Created by Charley on 2018/11/1.
+//  Copyright © 2018 VistaTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
